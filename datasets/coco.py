b25272a5e656aab7b46321b1552b1cd3499b9dc4
Suhail Najeeb <suhailnajeeb19@gmail.com>
2022-11-24 22:21:52 +1100

    update resize values

--- datasets/coco.py
+++ datasets/coco.py
@@ -112,6 +112,43 @@ class ConvertCocoPolysToMask(object):
         return image, target
 
 
+# def make_coco_transforms(image_set):
+
+#     normalize = T.Compose([
+#         T.ToTensor(),
+#         T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+#     ])
+
+#     scales = [480, 512, 544, 576, 600] # 608, 640, 672, 704, 736, 768, 800]
+
+#     if image_set == 'train':
+#         return T.Compose([
+#             T.RandomHorizontalFlip(),
+#             T.RandomSelect(
+#                 T.RandomResize(scales, max_size=600),                           # make change here!
+#                 T.Compose([
+#                     T.RandomResize([400, 500, 600]),
+#                     T.RandomSizeCrop(384, 600),
+#                     T.RandomResize(scales, max_size=600),                       # made change here
+#                 ])
+#             ),
+#             normalize,
+#         ])
+
+#     if image_set == 'val':
+#         return T.Compose([
+#             T.RandomResize([600], max_size=600),
+#             normalize,
+#         ])
+    
+#     if image_set == 'test':
+#         return T.Compose([
+#             T.RandomResize([600], max_size=600),
+#             normalize,
+#         ])
+
+#     raise ValueError(f'unknown {image_set}')
+
 def make_coco_transforms(image_set):
 
     normalize = T.Compose([
@@ -119,17 +156,17 @@ def make_coco_transforms(image_set):
         T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ])
 
-    scales = [480, 512, 544, 576, 600] # 608, 640, 672, 704, 736, 768, 800]
+    scales = [480, 512, 544, 576, 608, 640, 672, 704, 736, 768, 800]
 
     if image_set == 'train':
         return T.Compose([
             T.RandomHorizontalFlip(),
             T.RandomSelect(
-                T.RandomResize(scales, max_size=600),                           # make change here!
+                T.RandomResize(scales, max_size=1333),
                 T.Compose([
                     T.RandomResize([400, 500, 600]),
                     T.RandomSizeCrop(384, 600),
-                    T.RandomResize(scales, max_size=600),                       # made change here
+                    T.RandomResize(scales, max_size=1333),
                 ])
             ),
             normalize,
@@ -137,18 +174,17 @@ def make_coco_transforms(image_set):
 
     if image_set == 'val':
         return T.Compose([
-            T.RandomResize([600], max_size=600),
+            T.RandomResize([800], max_size=1333),
             normalize,
         ])
     
     if image_set == 'test':
         return T.Compose([
-            T.RandomResize([600], max_size=600),
+            T.RandomResize([800], max_size=1333),
             normalize,
         ])
 
     raise ValueError(f'unknown {image_set}')
-
 
 def build(image_set, args):
     root = Path(args.coco_path)
