58067bf755216d7f055c9db77f11e897c38b3032
Suhail Najeeb <suhailnajeeb19@gmail.com>
2022-12-19 17:52:16 +1100

    script for parsing outputs

---
+++ parse_country_scores.py
@@ -0,0 +1,24 @@
+from prettytable import PrettyTable
+
+table_name = 'table_name.csv'
+output_path = 'output'
+
+with open(output_path, 'r') as f:
+    output = f.readlines()
+
+table = PrettyTable(['Country', 'mAP', 'mAP50'])
+
+for line in output:
+    if "Processing data for:" in line: 
+        country = line.split(': ')[1][:-1]
+    if 'Average Precision  (AP) @[ IoU=0.50:0.95 | area=   all | maxDets=100 ]' in line:
+        map50 = line[-6:-1]
+    if 'Average Precision  (AP) @[ IoU=0.50      | area=   all | maxDets=100 ]' in line:
+        mapAll = line[-6:-1]
+        table.add_row([country, mapAll, map50])
+
+print(table)
+
+# Save th table to a CSV file: 
+with open(table_name, 'w') as f:
+    table.write_csv()
